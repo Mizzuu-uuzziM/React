9c446e154924d17373197f8002d605334db9284f
Mizzuu-uuzziM <fhmieynjb123@gmail.com>
2025-07-07 00:06:06 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Tabs from './components/Tabs';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>Website dengan 2 Tab</h1>
+        <Tabs />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
