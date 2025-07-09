534808cda6965c333a5ce661ddeef3805c300ad4
Mizzuu-uuzziM <fhmieynjb123@gmail.com>
2025-07-09 13:15:23 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/App.css';
+import App from './App/App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
